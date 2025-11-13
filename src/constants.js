8c4fbcc3f2bd267825268616914380f463fcfe87
muttasim Billah <muttasimbillah21@gmail.com>
2025-11-13 20:06:55 +0600

    portfolio website

---
+++ src/constants.js
@@ -0,0 +1,140 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+
+
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+// experience demo file
+
+  // export const experiences = [
+  //   {
+  //     id: 0,
+  //     img: webverseLogo,
+  //     role: "Fullstack Developer",
+  //     company: "Webverse Digital",
+  //     date: "April 2024 - Present",
+  //     desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+  //     skills: [
+  //       "HTML",
+  //       "CSS",
+  //       "JavaScript",
+  //       "React JS",
+  //       "TypeScript",
+  //       "Node JS",
+  //       "Tailwind CSS",
+  //       "MongoDb",
+  //       "Redux",
+  //       " Next Js",
+  //     ],
+  //   },
+
+  // ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://i.ibb.co.com/F4ynmYdZ/image.png",
+      school: "GLA University, Mathura",
+      date: "January 2016 - April 2021",
+      grade: "4.78 GPA",
+      desc: "I successfully completed my Secondary School Certificate (SSC) at Hazrabari High School, where I gained valuable knowledge and skills that laid the foundation for my future academic and professional endeavors.",
+      degree: "Secondary School Certificate - SSC",
+    },
+    {
+      id: 1,
+      img: "https://i.ibb.co.com/KpQH3GVs/image.png",
+      school: "Sherpur Polytechnic Institute",
+      date: "March 2022 - March 2026",
+      grade: "3.50 CGPA",
+      desc: "I have successfully completed a Diploma in Computer Science and Technology from Sherpur Polytechnic Institute. During my studies, I developed a strong foundation in programming, software development, and IT solutions, equipping me with the skills needed to pursue a career in the rapidly evolving tech industry.",
+      degree: "Diploma in Computer Science & Technology",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co.com/Lz5RSRYY/image.png",
+      school: "Bd Calling Academy ",
+      date: "September 2025 - January 2026",
+      grade: "Industrial Attachment",
+      desc: "I have successfully completed my industrial attachment at BDCalling Academy, where I specialized in MERN stack front-end development. During this attachment, I gained hands-on experience with technologies like React.js, MongoDB, Express.js, and Node.js, working on real-world projects to improve my skills in building dynamic, responsive web applications. This experience allowed me to bridge the gap between academic knowledge and practical industry skills, preparing me for a career as a front-end developer.",
+      degree: "MERN, FrontEnd Development",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Real state Management Project",
+      description:
+        "Welcome to our real estate website, where finding your dream home or commercial property is just a click away. We offer a wide range of properties, including apartments, houses, commercial office spaces, land, and more. Our platform is user-friendly, secure, and fast, providing you with an easy way to search and explore properties that fit your needs. Plus, our team of experienced real estate advisors is here to guide you through every step of the process, ensuring you make the best decision for your next investment or home.",
+      image: 'https://i.ibb.co.com/20S4Qw9s/image.png',
+      tags: ["HTML", "CSS", "JavaScript", "React JS","Tailwind", "React Router"],
+      github: "https://github.com/Billah72/real-eastate",
+      webapp: "https://estate-billah72.netlify.app/",
+    },
+  ];  
